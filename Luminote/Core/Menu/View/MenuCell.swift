f3b4fa98b8c4b11935fc13ba82834ac6309b7794
tobbbi <tobias.knoeppel@gmail.com>
2025-01-11 17:12:13 +0100

    Little bit of this, little bit of that.

---
+++ Luminote/Core/Menu/View/MenuCell.swift
@@ -0,0 +1,18 @@
+//
+//  MenuCell.swift
+//  Luminote
+//
+//  Created by Tobias Knoeppel on 11.01.25.
+//
+
+import SwiftUI
+
+struct MenuCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MenuCell()
+}
